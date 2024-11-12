4aaa951cdc5253065c700173b6c0a9f366e648de
Ben Liang <benliang03@gmail.com>
2024-11-11 17:40:35 -0800

    Initial setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Space Wars
+
+## Setup
+
+1. Clone the repository
+2. Run the following:
+
+```bash
+  npm init -y
+  npm install --save three
+  npm install --save-dev vite
+```
---
+++ main.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 10, 20);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.enabled = true;
+controls.minDistance = 10;
+controls.maxDistance = 50;
+
+function translationMatrix(tx, ty, tz) {
+	return new THREE.Matrix4().set(
+		1, 0, 0, tx,
+		0, 1, 0, ty,
+		0, 0, 1, tz,
+		0, 0, 0, 1
+	);
+}
+
+function rotationMatrixX(theta) {
+    return new THREE.Matrix4().set(
+        1, 0, 0, 0,
+        0, Math.cos(theta), -Math.sin(theta), 0,
+        0, Math.sin(theta), Math.cos(theta), 0,
+        0, 0, 0, 1
+    );
+}
+
+function rotationMatrixY(theta) {
+    return new THREE.Matrix4().set(
+        Math.cos(theta), 0, Math.sin(theta), 0,
+        0, 1, 0, 0,
+        -Math.sin(theta), 0, Math.cos(theta), 0,
+        0, 0, 0, 1
+    );
+}
+
+function rotationMatrixZ(theta) {
+	return new THREE.Matrix4().set(
+		Math.cos(theta), -Math.sin(theta), 0, 0,
+		Math.sin(theta),  Math.cos(theta), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	);
+}
+
+let geometry = new THREE.SphereGeometry(1, 32, 32);
+let material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+let sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+
+animate();
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
